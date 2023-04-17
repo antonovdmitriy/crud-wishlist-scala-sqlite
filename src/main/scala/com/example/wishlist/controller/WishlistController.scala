6878dea78037ccf270ed7d57f61fac33d346d854
Dmitriy Antonov <antonov.buscorr@gmail.com>
2023-04-17 21:13:59 +0200

    #8 crud service with implementation based on java class (#9)
    * #8 crud service with implementation based on java class

---
+++ src/main/scala/com/example/wishlist/controller/WishlistController.scala
@@ -0,0 +1,48 @@
+package com.example.wishlist.controller
+
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import com.example.wishlist.model.WishlistItemInput
+import com.example.wishlist.model.WishlistJsonProtocol._
+import com.example.wishlist.service.WishlistService
+
+class WishlistController(wishlistService: WishlistService) {
+  // Define the routes for the controller
+  val route: Route = pathPrefix("wishlist") {
+    pathEndOrSingleSlash {
+      get {
+        val items = wishlistService.getAllItems
+        complete(items)
+      } ~
+        post {
+          entity(as[WishlistItemInput]) { itemInput =>
+            val item = wishlistService.addItem(itemInput)
+            complete(StatusCodes.Created, item)
+          }
+        }
+    } ~
+      path(IntNumber) { id =>
+        get {
+          wishlistService.getItem(id) match {
+            case Some(item) => complete(item)
+            case None       => complete(StatusCodes.NotFound)
+          }
+        } ~
+          put {
+            entity(as[WishlistItemInput]) { itemInput =>
+              wishlistService.updateItem(id, itemInput) match {
+                case Some(item) => complete(item)
+                case None       => complete(StatusCodes.NotFound)
+              }
+            }
+          } ~
+          delete {
+            wishlistService.deleteItem(id) match {
+              case Some(item) => complete(item)
+              case None       => complete(StatusCodes.NotFound)
+            }
+          }
+      }
+  }
+}
---
+++ src/main/scala/com/example/wishlist/service/WishlistService.scala
@@ -0,0 +1,15 @@
+package com.example.wishlist.service
+
+import com.example.wishlist.model.{WishlistItem, WishlistItemInput}
+
+trait WishlistService {
+  def getAllItems: Seq[WishlistItem]
+
+  def getItem(id: Int): Option[WishlistItem]
+
+  def addItem(itemInput: WishlistItemInput): WishlistItem
+
+  def updateItem(id: Int, itemInput: WishlistItemInput): Option[WishlistItem]
+
+  def deleteItem(id: Int): Option[WishlistItem]
+}
