eb87b8c2987e2ebde79b14d5469826d1568fbcea
Dmitrii_Antonov <antonov.buscorr@gmail.com>
2023-04-18 14:57:10 +0200

    #2 ci

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,11 +2,14 @@ import akka.actor.CoordinatedShutdown
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpMethods.GET
+import akka.http.scaladsl.model.{HttpRequest, Uri}
 import akka.http.scaladsl.server.Route
 import com.example.wishlist.controller.WishlistController
 import com.example.wishlist.service.WishlistServiceImpl
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, ExecutionContext}
 import scala.io.StdIn
 
 object Main {
@@ -30,6 +33,12 @@ object Main {
         s"Server online at http://${binding.localAddress.getHostString}:${binding.localAddress.getPort}/"
       )
     }
+
+
+    // Send a GET request to /health endpoint
+    val request = HttpRequest(GET, uri = Uri("http://127.0.0.1:3000/health"))
+    val responseFuture = Http().singleRequest(request)
+    println(Await.result(responseFuture, 5.seconds))
 
     CoordinatedShutdown(system).addJvmShutdownHook {
       println("Server is shutting down...")
