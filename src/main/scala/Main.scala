f117771b8a661fe3fb5a40ec1ae7877d753b5ba3
Dmitrii_Antonov <antonov.buscorr@gmail.com>
2023-04-19 01:14:05 +0200

    #2 ci

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,32 +1,23 @@
 import akka.actor.CoordinatedShutdown
-import akka.actor.CoordinatedShutdown.UnknownReason
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.HttpMethods.GET
-import akka.http.scaladsl.model.{HttpRequest, Uri}
 import akka.http.scaladsl.server.Route
 import com.example.wishlist.controller.WishlistController
 import com.example.wishlist.service.WishlistServiceImpl
 
-import java.time.Instant
-import scala.concurrent.duration.{Duration, DurationInt}
+import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext}
-import scala.io.StdIn
 
 object Main {
   def main(args: Array[String]): Unit = {
     // Instantiate dependencies
     val wishlistService = new WishlistServiceImpl
-
-    // needed to run the route
-    implicit val system: ActorSystem[_] =
-      ActorSystem(Behaviors.empty, "wishlist-system")
-    // needed for the future flatMap/onComplete in the end
-    implicit val executionContext: ExecutionContext = system.executionContext
-
     val route: Route = new WishlistController(wishlistService).route
 
+    implicit val system: ActorSystem[_] =
+      ActorSystem(Behaviors.empty, "wishlist-system")
+    implicit val executionContext: ExecutionContext = system.executionContext
     val bindingFuture = Http().newServerAt("127.0.0.1", 3000).bind(route)
 
     // Log the server startup message
@@ -36,39 +27,6 @@ object Main {
       )
     }
 
-//    println("before sleep " + Instant.now() + " " + Thread.currentThread().getId)
-//
-//    Thread.sleep(5000)
-//
-//    println("after sleep " + Instant.now() + " " + Thread.currentThread().getId)
-
-    // Send a GET request to /health endpoint
-//    val request = HttpRequest(GET, uri = Uri("http://127.0.0.1:3000/health"))
-//    val responseFuture = Http().singleRequest(request)
-//    println(Await.result(responseFuture, 5.seconds))
-
-//    CoordinatedShutdown(system).addJvmShutdownHook {
-//      println("Server is shutting down...")
-//    }
-
-//    // Block until the server is shut down
-//    println(
-//      "before readline " + Instant.now() + " " + Thread.currentThread().getId
-//    )
-//    StdIn.readLine()
-//
-//    println(
-//      "after readline " + Instant.now() + " " + Thread.currentThread().getId
-//    )
-//
-//    bindingFuture
-//      .flatMap(_.unbind())
-//      .onComplete(_ => {
-//        println(
-//          "before terminate " + Instant.now() + " " + Thread.currentThread().getId
-//        )
-//        system.terminate()
-//      })
     // Register a JVM shutdown hook for coordinated shutdown
     CoordinatedShutdown(system).addJvmShutdownHook {
       println("Server is shutting down...")
--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -1,98 +1,29 @@
-import akka.http.scaladsl.model.HttpMethods.GET
-import akka.http.scaladsl.model.{HttpRequest, Uri}
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpMethods.GET
 import akka.http.scaladsl.model.StatusCodes.OK
+import akka.http.scaladsl.model.{HttpRequest, Uri}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-import java.time.Instant
-import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
 
 class MainSpec extends AnyWordSpec with Matchers with ScalatestRouteTest {
-//
-//  "Main" should "start and stop the server" {
-//    val thread = new Thread(() => Main.main(Array.empty))
-//    thread.start()
-//    Thread.sleep(1000) // Wait for server to start
-//
-//    // Perform your tests here, e.g. send HTTP requests and assert the responses
-//
-//    // Stop the server
-//    thread.interrupt()
-//    thread.join(5000) // Wait for server to stop
-//  }
 
   "Main" should {
 
     "start the app" in {
 
-//      val thread = new Thread(() => Main.main(Array.empty))
-//      thread.start()
-//      Thread.sleep(1000) // Wait for server to start
-//
-//            println(
-//              "before http request from test " + Instant
-//                .now() + " " + Thread.currentThread().getId
-//            )
-//
-//            // Send a GET request to /health endpoint
-//            val request = HttpRequest(GET, uri = Uri("http://127.0.0.1:3000/health"))
-//            val responseFuture = Http().singleRequest(request)
-//
-//            // Await the response and check the status code
-//            val response = Await.result(responseFuture, 5.seconds)
-//
-//            println(
-//              "after http request from test " + Instant
-//                .now() + " " + Thread.currentThread().getId
-//            )
-//
-//      // Stop the server
-//      thread.interrupt()
-//      thread.join(5000) // Wait for server to stop
-//
-//      response.status shouldBe OK
-
-
       val f = Future {
         Main.main(Array.empty)
       }
 
-      // Wait for some time for the server to start up
-
-//      println(
-//        "before sleep " + Instant.now() + " " + Thread.currentThread().getId
-//      )
-//
-//      Thread.sleep(5000)
-//
-//      println(
-//        "after sleep " + Instant.now() + " " + Thread.currentThread().getId
-//      )
-
-      println(
-        "before http request from test " + Instant
-          .now() + " " + Thread.currentThread().getId
-      )
-
-      // Send a GET request to /health endpoint
       val request = HttpRequest(GET, uri = Uri("http://127.0.0.1:3000/health"))
       val responseFuture = Http().singleRequest(request)
-
-      // Await the response and check the status code
       val response = Await.result(responseFuture, 5.seconds)
 
-      println(
-        "after http request from test " + Instant
-          .now() + " " + Thread.currentThread().getId
-      )
-
       response.status shouldBe OK
-
     }
-
   }
-
 }
